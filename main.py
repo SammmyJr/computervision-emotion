a75546bb61abf0d0da83795953bf92bb5a098852
Samuel <sammmyjr@hey.com>
2026-06-06 01:32:46 +1000

    Added CNN class, setup for macbooks specifically.

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ import torchvision.transforms as transforms
 
 import torchmetrics
 
+from cnn import CNN
+
+# Device setup
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 
 # Datasets
 # Using ImageFolder because it automatically maps subfolders as classes! Handy!
@@ -24,10 +28,16 @@ train_dataset = datasets.ImageFolder(
     root="dataset/train", transform=transforms.ToTensor()
 )
 
-test_dataset = datasets.ImageFolder(root="datasettest", transform=transforms.ToTensor())
+test_dataset = datasets.ImageFolder(
+    root="dataset/test", transform=transforms.ToTensor()
+)
 
 # Dataloaders
 batch_size = 60
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+# Load CNN
+model = CNN(in_channels=1, num_classes=7).to(device)
+print(model)
